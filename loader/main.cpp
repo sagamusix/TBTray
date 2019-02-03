3dec0cafa851d1c5829495e8048a72b9f779941c
Joachim Fenkes <github@dojoe.net>
2019-02-03 15:59:49 +0100

    Add a bunch of comments

--- loader/main.cpp
+++ loader/main.cpp
@@ -1,9 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*
+ * Traktor Touch loader
+ *
+ * Finds the active Traktor window, or starts Traktor if there is no window yet,
+ * then loads the companion DLL into Traktor via a Windows hook.
+ *
+ * Copyright (c) 2019 by Joachim Fenkes <github@dojoe.net>
+ */
+
+#define _CRT_SECURE_NO_WARNINGS    
 #include "windows.h"
 
 char dllName[2048];
 const char *appName = "Traktor Touch";
 
+char *GetErrorMessage()
+{
+	static char msgbuf[1024];
+	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msgbuf, sizeof(msgbuf), NULL);
+	return msgbuf;
+}
+
+/*
+ * Find the active Traktor window, or launch Traktor if no window is found.
+ */
 HWND getTraktor(LPSTR lpCmdLine)
 {
 	/* Try to find a running instance of Traktor */
@@ -35,16 +54,9 @@ HWND getTraktor(LPSTR lpCmdLine)
 	return 0;
 }
 
-char *GetErrorMessage()
-{
-	static char msgbuf[1024];
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msgbuf, sizeof(msgbuf), NULL);
-	return msgbuf;
-}
-
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	/* Construct the DLL filename from our own filename, then attempt to load the DLL and find the entry hook */
 	int dllNameLen = GetModuleFileName(NULL, dllName, sizeof(dllName));
 	strcpy(dllName + dllNameLen - 3, "dll");
 	HMODULE dll = LoadLibrary(dllName);
@@ -54,6 +66,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 1;
 	}
 
+	/* Grab Traktor and install the entry hook */
 	HWND hTraktorWindow = getTraktor(lpCmdLine);
 	DWORD idTraktorUIThread  = GetWindowThreadProcessId(hTraktorWindow, NULL);
 	HHOOK hook = SetWindowsHookEx(WH_CALLWNDPROC, hookProc, dll, idTraktorUIThread);
@@ -62,11 +75,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 1;
 	}
 	FreeLibrary(dll);
+
+	/* Send a message to the hooked WindowProc to make sure the DLL install code ran before we exit */
 	SendMessage(hTraktorWindow, WM_NULL, 0, 0);
 
 #ifdef _DEBUG
 	MessageBox(0, "DLL injected - hit OK to unload", appName, MB_ICONINFORMATION);
 
+	/* Tell the DLL to unhook and unload itself */
 	if (0xBABE != SendMessage(hTraktorWindow, WM_APP, 0xCAFE, 0xDEADBEEF))
 		MessageBox(0, "Failed to unhook", "Yikes", MB_ICONEXCLAMATION);
 	UnhookWindowsHookEx(hook);
