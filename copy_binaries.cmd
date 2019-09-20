5fa31854aa4f88575b86a1de95c53049ee775bcf
Johannes Schultz <git42@sagagames.de>
2019-09-20 22:37:54 +0200

    Add test files to bin folder

--- copy_binaries.cmd
+++ copy_binaries.cmd
@@ -1,5 +1,7 @@
 copy Release\dll.dll bin\TBTray.dll
 copy Release\loader.exe bin\TBTray.exe
+copy README.md bin
+copy LICENSE.txt bin
 echo TBTray register > bin\register.cmd
 echo TBTray unregister > bin\unregister.cmd
 pause
